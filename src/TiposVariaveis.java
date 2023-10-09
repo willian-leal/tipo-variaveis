a94efc6eaea2da19064a9ae43311cd8d28161538
willian-leal <willian.oliveira@bandtec.com.br>
2023-10-09 19:29:28 -0300

    code review

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,5 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
