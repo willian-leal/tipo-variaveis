db8686cd245f18658b22185ae2cba7861ee54149
willian-leal <willian.oliveira@bandtec.com.br>
2023-10-12 14:24:10 -0300

    #

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -7,8 +7,6 @@ public class TiposVariaveis {
         int numero = 5;
         numero = 10;
 
-        // para que uma variável seje uma constante precisa da palavra reservada 'final'
-        // e o nome dever ser declarado em upercase
         final double VALOR_DE_PI = 3.14;
         System.out.print(VALOR_DE_PI);
     }
