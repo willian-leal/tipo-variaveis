805a545d4c83791a38cf6a5271e60c48d724b8e3
willian-leal <willian.oliveira@bandtec.com.br>
2023-10-12 14:22:18 -0300

    #

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello, Willian");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        // para que uma variável seje uma constante precisa da palavra reservada 'final'
+        // e o nome dever ser declarado em upercase
+        final double VALOR_DE_PI = 3.14;
+        System.out.print(VALOR_DE_PI);
     }
 }
